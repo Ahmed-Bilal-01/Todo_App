6764e0291b3b07a61a95d35e993c2c7f696ef73f
Ahmed Bilal <137169507+Ahmed-Bilal-01@users.noreply.github.com>
2024-06-29 14:29:53 +0500

    safearea

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 
 const App = () => {
-  return <AppNavigator />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <AppNavigator />
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
